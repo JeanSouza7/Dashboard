61dbbda57d077334c4b4651ec70c6f2c59d2d3dd
JeanSouza7 <jeean09.souza@gmail.com>
2026-05-12 19:56:52 -0300

    Filtros alterados

--- app.py
+++ app.py
@@ -8,10 +8,42 @@ import apis
 
 
 st.set_page_config(page_title="Games Dashboard", page_icon="🎮", layout="wide")
+# =========================
+# CUSTOMIZAÇÃO DE CORES
+# =========================
+st.markdown("""
+<style>
 
+/* Tags do multiselect */
+.stMultiSelect [data-baseweb="tag"] {
+    background-color: #3b82f6 !important; /* azul */
+    color: white !important;
+    border-radius: 8px !important;
+}
 
+/* X do botão */
+.stMultiSelect [data-baseweb="tag"] svg {
+    fill: white !important;
+}
+
+/* Slider */
+.stSlider div[data-baseweb="slider"] div {
+    color: #3b82f6 !important;
+}
+
+/* Bolinha do slider */
+.stSlider [role="slider"] {
+    background-color: #3b82f6 !important;
+}
+
+/* Barra preenchida */
+.stSlider div[data-testid="stTickBar"] div {
+    background-color: #3b82f6 !important;
+}
+
+</style>
+""", unsafe_allow_html=True)
 database.criar_banco()
-
 
 st.sidebar.title("🎮 Games Dashboard")
 st.sidebar.markdown("---")
@@ -30,6 +62,13 @@ st.sidebar.markdown("---")
 st.sidebar.caption("Fontes: SteamSpy · RAWG · FreeToGame · CheapShark")
 
 df = database.ler()
+# Limpeza dos gêneros
+df["genero"] = (
+    df["genero"]
+    .fillna("N/A")
+    .astype(str)
+    .str.strip()
+)
 
 st.title("🎮 Games Dashboard")
 st.caption("Dados de jogos obtidos via APIs públicas e armazenados localmente.")
@@ -40,11 +79,148 @@ if df.empty:
     st.stop()
 
 
-fontes = ["Todas"] + sorted(df["fonte"].unique().tolist())
-fonte_sel = st.selectbox("Filtrar por fonte:", fontes)
+st.sidebar.header("🎯 Filtros")
+# =========================
+# FONTES
+# =========================
 
-if fonte_sel != "Todas":
-    df = df[df["fonte"] == fonte_sel]
+fontes_sel = st.sidebar.multiselect(
+    "📡 Fontes",
+    df["fonte"].unique(),
+    default=df["fonte"].unique()
+)
+
+df = df[df["fonte"].isin(fontes_sel)]
+# =========================
+# GÊNERO
+# =========================
+
+# =========================
+# GÊNERO
+# =========================
+
+# Separar gêneros compostos
+todos_generos = set()
+
+for lista in df["genero"].dropna():
+
+    for g in str(lista).split(","):
+
+        g = g.strip()
+
+        if g:
+            todos_generos.add(g)
+
+generos = sorted(todos_generos)
+
+gen_sel = st.sidebar.selectbox(
+    "🎮 Gênero",
+    ["Todos"] + generos
+)
+
+if gen_sel != "Todos":
+
+    if gen_sel == "N/A":
+        df = df[df["genero"] == "N/A"]
+
+    else:
+        df = df[
+            df["genero"].str.contains(
+                rf"\b{gen_sel}\b",
+                case=False,
+                na=False,
+                regex=True
+            )
+        ]
+# =========================
+# AVALIAÇÃO
+# =========================
+
+fontes_com_avaliacao = ["SteamSpy", "RAWG"]
+
+mostrar_avaliacao = any(
+    fonte in fontes_com_avaliacao
+    for fonte in fontes_sel
+)
+
+if mostrar_avaliacao and not df.empty:
+
+    max_av = int(df["avaliacao"].max())
+
+    if max_av > 0:
+
+        nota_min = st.sidebar.slider(
+            "⭐ Avaliação mínima",
+            min_value=0,
+            max_value=max_av,
+            value=0
+        )
+
+        if nota_min > 0:
+            df = df[
+                (~df["fonte"].isin(fontes_com_avaliacao)) |
+                (df["avaliacao"] >= nota_min)
+            ]
+
+# =========================
+# JOGADORES
+# =========================
+fontes_com_jogadores = ["SteamSpy", "RAWG"]
+
+mostrar_jogadores = any(
+    fonte in fontes_com_jogadores
+    for fonte in fontes_sel
+)
+
+if mostrar_jogadores and not df.empty:
+
+    max_jog = int(df["jogadores"].max())
+
+    # Evita erro do slider
+    if max_jog > 0:
+
+        min_jog = st.sidebar.slider(
+            "👥 Jogadores mínimos",
+            min_value=0,
+            max_value=max_jog,
+            value=0
+        )
+
+        if min_jog > 0:
+            df = df[
+                (~df["fonte"].isin(fontes_com_jogadores)) |
+                (df["jogadores"] >= min_jog)
+            ]
+
+# =========================
+# ORDENAÇÃO
+# =========================
+
+ordem = st.sidebar.selectbox(
+    "🔽 Ordenar por",
+    ["Jogadores", "Avaliação", "Nome"]
+)
+
+if ordem == "Jogadores":
+    df = df.sort_values("jogadores", ascending=False)
+
+elif ordem == "Avaliação":
+    df = df.sort_values("avaliacao", ascending=False)
+
+else:
+    df = df.sort_values("nome")
+
+df = df.reset_index(drop=True)
+
+
+if ordem == "Jogadores":
+    df = df.sort_values("jogadores", ascending=False)
+
+elif ordem == "Avaliação":
+    df = df.sort_values("avaliacao", ascending=False)
+
+else:
+    df = df.sort_values("nome")
 
 df = df.reset_index(drop=True)
 
